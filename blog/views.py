1be665e6f1e8972d7a7077ce0bed7d590b02ee76
Sofia <giraldosofia481@gmail.com>
2023-11-29 19:16:27 -0500

    Algunos problemas

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
-def post_detail(request, pk):
+def post_list(request,pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, "blog/post_detail.html", {'post': post})
+    # return render(request, 'blog/post_list.html', {})
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, "blog/post_detail.html", {'post': post})
